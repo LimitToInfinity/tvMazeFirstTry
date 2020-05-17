0a873ccd59d8816604cf0bcbaa7e66cf76d56d3e
LimitToInfinity <21stLimitless@gmail.com>
2020-05-17 02:21:37 -0600

    Updates range from 1 to 191 for deploy.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
     flattenResponses
 } from "./utilities.js";
 
-const apiShowsPages = createRangeFromTo(0, 0);
+const apiShowsPages = createRangeFromTo(0, 191);
 const fetchCalls = apiShowsPages.map(makeFetchCalls);
 
 Promise.all(fetchCalls)
