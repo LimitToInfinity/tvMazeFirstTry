8271a8aa32f2826ed624a3e34002cf1337ca8093
LimitToInfinity <21stLimitless@gmail.com>
2020-05-18 00:33:06 -0600

    Clears web network search value, and removes label shrink class when remove filters pill is selected.

--- src/genreSelector.js
+++ src/genreSelector.js
@@ -60,6 +60,8 @@ export class GenreSelector {
         if (selectedGenre === "Remove filters!") {
             APP_STATE.searchBar.element.value = "";
             APP_STATE.webNetworkSelector.selectedWebNetwork = undefined;
+            APP_STATE.webNetworkSelector.webNetworksSearch.value = "";
+            APP_STATE.webNetworkSelector.removeShrinkClass();
             this.selectedGenres.clear();
         }
 
